16e943bcabb5554088649063053a44228d6d5b39
Katya Mikanov <kategubko@gmail.com>
2023-05-01 18:57:28 +0300

    feat: add texterea

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //virtual keyboard
+
+const screen = document.querySelector ('body');
+
+
+function createKeyBoard () {
+    let  board = document.createElement("TEXTAREA");;
+    board.classList.add('screen');
+
+
+    screen.append(board);
+}
+
+createKeyBoard ();
